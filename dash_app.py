feeeb9dc326f7ec7a59281991b28f6641f6a228c
nnpmpt <nannapat2342@gmail.com>
2025-04-24 14:13:35 +0700

    basic dash

---
+++ dash_app.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html
+
+#### create app ####
+app = dash.Dash(__name__)
+app.title = 'Test'
+
+#### define layout ####
+app.layout = html.Div("Hello World")
+
+#### run the app ####
+if __name__ == '__main__':
+    app.run_server(debug=True, port=5000)
